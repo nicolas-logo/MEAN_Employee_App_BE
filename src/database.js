bdd7dfd8d91373e305ba3f1842d2b4ae38445e8b
Nicolás Logosettis <logo.ncls@gmail.com>
2023-07-02 20:53:04 -0300

    local uri added

--- src/database.js
+++ src/database.js
@@ -1,5 +1,5 @@
 const mongoose = require('mongoose');
-
+console.log("Connecting to ",process.env.MONGODB_URI_LOCAL)
 mongoose.connect(process.env.MONGODB_URI)
     .then(db => console.log("Db is connected"))
     .catch(err => console.error(err))
